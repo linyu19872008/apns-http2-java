d9d9034fd017bcccc7aba7913c5e3b90a5b5250e
jacklin <liny@chuchujie.com>
2017-05-04 10:19:31 +0800

    添加说明文档

--- apns-http2-core/src/main/java/com/cdeer/apns/http2/core/service/NettyApnsService.java
+++ apns-http2-core/src/main/java/com/cdeer/apns/http2/core/service/NettyApnsService.java
@@ -21,19 +21,33 @@ public class NettyApnsService extends AbstractApnsService {
 
     @Override
     public void sendNotification(PushNotification notification) {
-        executorService.execute(() -> {
-            NettyApnsConnection connection = null;
-            try {
-                connection = connectionPool.acquire();
-                if (connection != null) {
-                    connection.sendNotification(notification);
-                }
-            } catch (Exception e) {
-                log.error("sendNotification", e);
-            } finally {
-                connectionPool.release(connection);
+//        executorService.execute(() -> {
+//            NettyApnsConnection connection = null;
+//            try {
+//                connection = connectionPool.acquire();
+//                if (connection != null) {
+//                    connection.sendNotification(notification);
+//                }
+//            } catch (Exception e) {
+//                log.error("sendNotification", e);
+//            } finally {
+//                connectionPool.release(connection);
+//            }
+//        });
+
+
+        NettyApnsConnection connection = null;
+        try {
+            connection = connectionPool.acquire();
+            if (connection != null) {
+                connection.sendNotification(notification);
             }
-        });
+        } catch (Exception e) {
+            log.error("sendNotification", e);
+        } finally {
+            connectionPool.release(connection);
+        }
+
     }
 
     @Override
